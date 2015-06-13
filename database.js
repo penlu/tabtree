3b48d59c89e9794adbc04ba54d3ed432ce3acfc0
Eric Lu <penlume@gmail.com>
2015-06-13 17:28:13 -0400

    added database skeleton

---
+++ database.js
@@ -0,0 +1,39 @@
+// within a session, we associate tab IDs with node IDs
+tabassocs = {}
+
+/* add node to history tree
+ *
+ * url = the url of the new page
+ * time = the time at which the new page was loaded
+ * newtabid = the tab ID associated with this page
+ * origtabid = the tab ID that originated this page (may be left blank for root)
+ *
+ * the return value is the SHA1 hash associated with this node
+ */
+function addNode(url, time, newtabid, origtabid) {
+    // orig = the SHA1 hash associated with the originating page/timestamp
+}
+
+/* get the information associated with this node
+ * returns a JSON object:
+ * {
+ *   "url": the url,
+ *   "time": timestamp,
+ *   "children": [a list of child nodes]
+ * }
+ */
+function getNode(node) {
+    
+}
+
+/* get roots of browsing tree
+ * returns a list of parentless nodes
+ */
+function getRoots() {
+
+}
+
+function tabClose(tabid) {
+
+}
+
