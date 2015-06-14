1a911689772d10033d85e9ce7eed5a7fae34894a
Ian Zhou <ianzhou186@gmail.com>
2015-06-14 02:14:41 -0400

    shows url and indents children

--- history.js
+++ history.js
@@ -1,6 +1,25 @@
-var roots = getRoots();
-var mydiv = document.getElementById("text");
-var aTag = document.createElement("a'");
-aTag.setAttribute("href","http://www.google.com");
-aTag.innerHTML = "link text";
-mydiv.appendChild(aTag);
+getRoots(function (rootList){
+	alert(rootList.length)
+	for(i = 0; i < rootList.length; i++){
+		getNode(rootList[i], function (node){
+			addUrl(node, document.getElementsByTagName('body')[0], 0);
+		});
+	}
+});
+
+function addUrl(node, parentTag, indent){
+	var divTag = document.createElement("div");
+	var aTag = document.createElement("a");
+	divTag.style.textIndent = indent.toString() + "px";
+	aTag.setAttribute("href", node.url);
+	aTag.innerText = node.url;
+	divTag.appendChild(aTag);
+	parentTag.appendChild(divTag);
+	if(node.children.length != 0){
+		for(i = 0; i < node.children.length; i++){
+			getNode(node.children[i], function (childNode){
+				addUrl(childNode, divTag, indent + 10);
+			});
+		}
+	}
+}
